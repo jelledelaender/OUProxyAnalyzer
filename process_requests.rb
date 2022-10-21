54ff3e608e15d294f6df15cdcabc166b33cdc51d
Jelle De Laender <jelle@codingmammoth.com>
2022-10-21 22:01:17 +0200

    Adding validations, comments

--- process_requests.rb
+++ process_requests.rb
@@ -1,8 +1,45 @@
 #!/usr/bin/env ruby
 
+# process_requests.rb
+# 
+# Objective of process_rquests.rb is to analyze the CSV export of the proxy
+# The end result is an overview of the requests
+# Which domains were requested, was HTTPS used
+# How many times was the less secure HTTP used, and showing those URLs
+#
+# Via an optional parameter, an IP can be given to limit the results of this host
+# 
+# Usage
+# ruby process_requests.rb <csv_file.csv> <optional: filter_ip>
+# 
+
 require 'csv'
 require 'uri'
 
+## Validating parameters
+arg_length = ARGV.length
+if arg_length < 1 || arg_length > 2
+  puts "Incorrect usage."
+  puts "Usage: ruby process_requests.rb <csv_file.csv> <optional: filter_ip>"
+  exit
+end
+
+file = ARGV[0]
+
+if !File.exists?(file)
+  puts "No such file or directory"
+  exit
+end
+
+
+ip = false
+ip = ARGV[1] if arg_length > 1
+
+if ip
+  puts "Only processing requests from #{ip}"
+end
+
+## Initialising variables
 hosts = Hash.new(0)
 schemes = Hash.new(0)
 insecure_requests = Array.new
@@ -10,9 +47,8 @@ number_of_requests = 0
 
 # data = CSV.read("CancyCrush.csv")
 
-CSV.foreach(("example.csv"), headers: true, col_sep: ",") do |row|
-    number_of_requests += 1
-
+CSV.foreach((file), headers: true, col_sep: ",") do |row|
+    ## DataFields
     # URL
     # Status
     # Response Code
@@ -46,21 +82,26 @@ CSV.foreach(("example.csv"), headers: true, col_sep: ",") do |row|
     # Request Compression
     # Response Compression
 
+    if ip != false && row["Client Address"] != "/#{ip}" ## Skip entry if IP filter is active, and client-address is different
+        next
+    end
+
+    ## Processing entry
+    number_of_requests += 1
+
     url = row["URL"]
     
     uri = URI.parse(url)
-    
-#    uri.host.split('.').first
-    
+        
     schemes[uri.scheme] += 1
     hosts[uri.host] += 1
 
     insecure_requests.append url if uri.scheme == "http"
 end 
 
+## Sorting counters
 schemes = schemes.sort_by {|_key, value| -value}.to_h
 hosts = hosts.sort_by {|_key, value| -value}.to_h
-
 
 puts "Results:"
 puts "Total Requests: #{number_of_requests}"
